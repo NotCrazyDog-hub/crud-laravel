f7982789a38b00880b0bce67cf6bfb61e1f93cf4
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-10-28 20:39:28 -0300

    Todas as routes do CRUD de estagiários adicionadas

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,11 @@ use App\Http\Controllers\InternController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/interns', [InternController::class, 'index'])->name('interns.index');
+Route::get('/interns/create', [InternController::class, 'create'])->name('interns.create');
+Route::post('/interns', [InternController::class, 'store'])->name('interns.store');
+Route::get('/interns/{intern}', [InternController::class, 'show'])->name('interns.show');
+Route::get('/interns/{intern}/edit', [InternController::class, 'edit'])->name('interns.edit');
+Route::put('/interns/{intern}', [InternController::class, 'update'])->name('interns.update');
+Route::delete('/interns/{intern}', [InternController::class, 'destroy'])->name('interns.destroy');
